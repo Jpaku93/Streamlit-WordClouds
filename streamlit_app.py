98856b8b2f0699e172b86d85255d08906d551cf7
Johnson Paku <65574434+Jpaku93@users.noreply.github.com>
2023-11-30 14:53:14 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,10 @@ import math
 
 # upload csv file and read into dataframe
 st.title("Word Clouds")
+st.write("Drop the cluster csv in the app to generate wordclouds, also add the summary column if you want")
+st.write("Make sure to pick the write columns to process")
+st.write("upload csv file and read into dataframe")
+
 st.write("upload csv file and read into dataframe")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
